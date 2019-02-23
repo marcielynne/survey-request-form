eccff145ed503b68a5fb174ddd628564b5d228df
Marcie Deff <marcielynne@gmail.com>
2019-02-23 15:17:24 -0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg'; 
-import './App.css';
+import React from 'react';
+import './css/index.css';
+import EsriLoaderReact from 'esri-loader-react';
 
+class App extends React.PureComponent {
 
-  class App extends Component {
   render() {
+    const options = {
+      url: 'https://js.arcgis.com/4.6/'
+    };
+ 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <EsriLoaderReact 
+        options={options} 
+        modulesToLoad={[
+          'esri/Map', 
+          'esri/views/MapView',
+          'esri/Graphic',
+          'esri/symbols/SimpleMarkerSymbol',
+          'esri/symbols/SimpleLineSymbol',
+          'esri/Color',
+          'esri/geometry/support/webMercatorUtils',
+          'esri/geometry/Point'
+        ]}    
+        
+        onReady={({loadedModules: [Map, MapView, SimpleMarkerSymbol, SimpleLineSymbol, Color, webMercatorUtils, Point]}) => {
+          new MapView({
+            container: 'mapDiv',
+            center: [-95, 40],
+            zoom: 4,
+            map: new Map({
+              basemap: 'hybrid'
+            })
+          });
+        }}
+      />
     );
   }
 }
 
-export default App; 
+export default App;
+
