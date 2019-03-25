fdbe75ab9ffc483c113f838f8f99427c42abb722
Marcie Deff <marcielynne@gmail.com>
2019-03-25 08:35:43 -0400

    Updated link css

--- src/Nav.js
+++ src/Nav.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 // Navigation component. This incorporates Links used in React Router. 
 const Nav = () => (
     <div className="main-nav">
         <ul className="nav">
-            <li className="item" id="item-1"><Link exact="true" to="/">Main</Link></li>
-            <li className="item" id="item-2"><Link to="/projects">Projects</Link></li>
+            <li className="item" id="item-1"><NavLink exact to="/">Main</NavLink></li>
+            <li className="item" id="item-2"><NavLink to="/projects">Projects</NavLink></li>
             <li className="logo"><img src="img/3d-logo.png" alt="Company Logo" id="image" /></li>
-            <li className="item" id="item-3"><Link to="/search">Search</Link></li>
-            <li className="item" id="item-4"><Link to="/time">Time</Link></li>
+            <li className="item" id="item-3"><NavLink to="/search">Search</NavLink></li>
+            <li className="item" id="item-4"><NavLink to="/time">Time</NavLink></li>
         </ul>
     </div>
 );
